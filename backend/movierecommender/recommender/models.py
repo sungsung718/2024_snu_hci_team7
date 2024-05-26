d47e1b49983688f88a1ace8d2bf248d67ec2842d
Sungmin <sungsung718@gmail.com>
2024-05-27 01:34:32 +0900

    feat: Add Preference model

--- backend/movierecommender/recommender/models.py
+++ backend/movierecommender/recommender/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from jsonfield import JSONField
 # Create your models here.
+
+
+class Preference(models.Model):
+    genre = JSONField(null=True, blank=True)
+    director = models.CharField(max_length=100, null=True, blank=True)
+    actor = models.CharField(max_length=100, null=True, blank=True)
+    liked = models.CharField(max_length=100, null=True, blank=True)
+    hated = models.CharField(max_length=100, null=True, blank=True)
+    detail = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return (
+            f"Preference for {self.director or self.actor or self.liked or self.hated}"
+        )
