4d3f4bf34042f68ad7e3054cc106fcad304d2176
Sungmin <sungsung718@gmail.com>
2024-06-02 12:25:51 +0900

    feat: Add serializers for the feedback feature

--- backend/movierecommender/recommender/serializers.py
+++ backend/movierecommender/recommender/serializers.py
@@ -1,26 +1,49 @@
 from rest_framework import serializers
 from .models import Preference, Movie, Recommendation
+from .utils.utils import elems2int, str2arr
 
 
 class PreferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Preference
-        fields = '__all__'
+        fields = "__all__"
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'director', 'rating', 'detail', 'image', 'year']
+        fields = ["id", "title", "director", "rating", "detail", "image", "year"]
 
 
-class RecommendationSerializer(serializers.ModelSerializer):
+class RecommendationCreateSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
-        return {
-            'id': instance.id,
-            'movies': instance.movies
-        }
+        return {"id": instance.id, "movies": instance.movies}
 
     class Meta:
         model = Recommendation
-        fields = ['id', 'movies', 'preference']
+        fields = ["id", "movies", "preference"]
+
+
+class RecommendationRetrieveSerializer(serializers.ModelSerializer):
+    def get_movies(self):
+        return elems2int(str2arr(self.data["movies"]))
+
+    class Meta:
+        model = Recommendation
+        fields = ["id", "movies", "preference", "likes", "hates"]
+
+
+class RecommendationUpdateSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        return {"id": instance.id, "movies": instance.movies}
+
+    class Meta:
+        model = Recommendation
+        fields = ["id", "movies", "preference", "likes", "hates", "feedback_detail"]
+
+
+class FeedbackSerializer(serializers.Serializer):
+    recommendation_id = serializers.IntegerField()
+    likes = serializers.CharField(allow_blank=True)
+    hates = serializers.CharField(allow_blank=True)
+    detail = serializers.CharField(allow_blank=True)
---
+++ backend/movierecommender/recommender/utils/utils.py
@@ -0,0 +1,6 @@
+def str2arr(string, delimiter=","):
+    return string.split(delimiter)
+
+
+def elems2int(lst):
+    return list(map(int, lst))
