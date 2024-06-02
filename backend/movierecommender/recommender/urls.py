52b47ae7b5c5465be2026fefcb0153d4fc5d69f1
Sungmin <sungsung718@gmail.com>
2024-06-02 12:27:55 +0900

    feat: Add a url for RecommendationUpdateView

--- backend/movierecommender/recommender/urls.py
+++ backend/movierecommender/recommender/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from recommender.views import RecommendationCreateView
+from recommender.views import RecommendationCreateView, RecommendationUpdateView
 
 urlpatterns = [
-    path("recommendations", RecommendationCreateView.as_view())
+    path("recommendations", RecommendationCreateView.as_view()),
+    path("recommendations/<int:pk>", RecommendationUpdateView.as_view()),
 ]
