304cf0d14a76fe696956212ca387ebd723b2adad
Sungmin <sungsung718@gmail.com>
2024-06-03 15:07:21 +0900

    feat: Register url for PreviewRetrieveView

--- backend/movierecommender/recommender/urls.py
+++ backend/movierecommender/recommender/urls.py
@@ -4,10 +4,12 @@ from recommender.views import (
     RecommendationCreateView,
     RecommendationUpdateView,
     FinalRecommendationCreateView,
+    PreviewRetrieveView,
 )
 
 urlpatterns = [
     path("recommendations", RecommendationCreateView.as_view()),
     path("recommendations/<int:pk>", RecommendationUpdateView.as_view()),
     path("result", FinalRecommendationCreateView.as_view()),
+    path("preview", PreviewRetrieveView.as_view()),
 ]
