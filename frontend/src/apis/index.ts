38ba2122c25ef3e35c410fe65c89067e04328518
chansol <lchans0319@gmail.com>
2024-06-05 01:25:06 +0900

    fix: api url 수정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,6 +1,6 @@
 import { Movie, Preference } from "@/customTypes";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "http://localhost:8000/api";
 
 const getRequest = async <T = unknown>(url: string): Promise<T> => {
   const response = await fetch(`${BASE_URL}${url}`);
@@ -30,22 +30,22 @@ const putRequest = async <T = unknown>(
 };
 
 export const getPreviewMovies = async () => {
-  return {
-    recent_movies: [1, 2, 3, 4, 5].map((i) => ({
-      title: "액션" + i,
-      image:
-        "https://i.namu.wiki/i/kCl6N7uOwDN3EAzl7hRjouXiOPWajThFWy6V7BJ1s0W00k-LtKIXunQTIpd2npiN1hsPGr-XYWnE-uca4DiWnQ.webp",
-      year: 1984,
-      hashtags: "#통쾌한#대표 한국 범죄 영화#대중이 원하는 오락",
-    })),
-    classic_movies: [1, 2, 3, 4, 5].map((i) => ({
-      title: "액션" + i,
-      image:
-        "https://i.namu.wiki/i/kCl6N7uOwDN3EAzl7hRjouXiOPWajThFWy6V7BJ1s0W00k-LtKIXunQTIpd2npiN1hsPGr-XYWnE-uca4DiWnQ.webp",
-      year: 1984,
-      hashtags: "#통쾌한#대표 한국 범죄 영화#대중이 원하는 오락",
-    })),
-  };
+  // return {
+  //   recent_movies: [1, 2, 3, 4, 5].map((i) => ({
+  //     title: "액션" + i,
+  //     image:
+  //       "https://i.namu.wiki/i/kCl6N7uOwDN3EAzl7hRjouXiOPWajThFWy6V7BJ1s0W00k-LtKIXunQTIpd2npiN1hsPGr-XYWnE-uca4DiWnQ.webp",
+  //     year: 1984,
+  //     hashtags: "#통쾌한#대표 한국 범죄 영화#대중이 원하는 오락",
+  //   })),
+  //   classic_movies: [1, 2, 3, 4, 5].map((i) => ({
+  //     title: "액션" + i,
+  //     image:
+  //       "https://i.namu.wiki/i/kCl6N7uOwDN3EAzl7hRjouXiOPWajThFWy6V7BJ1s0W00k-LtKIXunQTIpd2npiN1hsPGr-XYWnE-uca4DiWnQ.webp",
+  //     year: 1984,
+  //     hashtags: "#통쾌한#대표 한국 범죄 영화#대중이 원하는 오락",
+  //   })),
+  // };
 
   return await getRequest<{
     recent_movies: Movie[];
