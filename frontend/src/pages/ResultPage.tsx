d48f75bb3aab6ee0269b07fe45b7ba1e6f167c0a
chansol <lchans0319@gmail.com>
2024-06-15 10:32:13 +0900

    fix: 이미지 저장 관련 코드 삭제

--- frontend/src/pages/ResultPage.tsx
+++ frontend/src/pages/ResultPage.tsx
@@ -1,12 +1,8 @@
 import { Link, useLocation } from "react-router-dom";
 
-import { toPng } from "html-to-image";
-// import { useScreenshot, createFileName } from "use-react-screenshot";
-
 import { Movie } from "@/customTypes";
 import MovieCard from "@/components/recommend/MovieCard";
-import { useCallback, useRef } from "react";
-import html2canvas from "html2canvas";
+import { useRef } from "react";
 
 export default function ResultPage() {
   const location = useLocation();
@@ -15,68 +11,6 @@ export default function ResultPage() {
   const history: string[] = [...location.state.history].reverse();
 
   const ticketRef = useRef<HTMLDivElement>(null);
-
-  // const [image, takeScreenshot] = useScreenshot({
-  //   type: "image/jpeg",
-  //   quality: 1.0,
-  // });
-
-  // const download = (
-  //   image: string | null,
-  //   { name = "img", extension = "jpg" } = {}
-  // ) => {
-  //   console.log("downlodad");
-  //   console.log(image);
-  //   if (!image) return;
-  //   const a = document.createElement("a");
-  //   a.href = image;
-  //   a.download = createFileName(extension, name);
-  //   a.click();
-  // };
-
-  // const handleSaveAsImage = async () => {
-  //   if (!myref || !myref.current) return;
-  //   await takeScreenshot(myref.current);
-  //   download(image);
-  // };
-
-  // const handleSaveAsImage = async () => {
-  // alert("Under development");
-  // return;
-  // };
-
-  // const handleSaveAsImage = useCallback(async () => {
-  //   if (ticketRef.current === null) {
-  //     alert("이미지를 저장할 수 없습니다.");
-  //     return;
-  //   }
-
-  //   toPng(ticketRef.current, { cacheBust: true })
-  //     .then((dataUrl) => {
-  //       const link = document.createElement("a");
-  //       link.download = "my-image-name.png";
-  //       link.href = dataUrl;
-  //       link.click();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, [ticketRef]);
-
-  const handleSaveAsImage = () => {
-    console.log("Asdfadf");
-    if (!ticketRef || !ticketRef.current) return;
-    console.log("Asdfadsdfsdfdf");
-
-    html2canvas(ticketRef.current, { proxy: "/html2canvas-proxy" }).then(
-      function (canvas) {
-        const downloadLink = document.createElement("a");
-        downloadLink.download = "filename.png";
-        downloadLink.href = canvas.toDataURL();
-        downloadLink.click();
-      }
-    );
-  };
 
   return (
     <div className="pt-[65px] flex flex-col items-center pb-[100px] min-h-full min-w-fit bg-[url('src/assets/beige_background.png')]">
@@ -102,14 +36,6 @@ export default function ResultPage() {
           <Movies movies={movies} />
         </div>
       </div>
-      {/* <button
-        className="bg-[#5D544C] rounded-full mt-10 w-[45px] h-[45px] flex justify-center items-center"
-        onClick={handleSaveAsImage}
-      >
-        <span className="material-symbols-outlined text-white font-light">
-          download_2
-        </span>
-      </button> */}
     </div>
   );
 }
